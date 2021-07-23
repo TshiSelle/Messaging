6ed63172a76fb39d78259147780b1f6e769fad60
OmarHoussami <omar.houssami20@gmail.com>
2021-07-23 19:01:21 +0300

    icon is now clickable

--- aloo-mern/src/Sidebar.jsx
+++ aloo-mern/src/Sidebar.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Sidebar.css';
+import AllOutIcon from '@material-ui/icons/AllOut';
+import { Avatar, IconButton } from '@material-ui/core';
 
 function Sidebar() {
   return (
     <div className='sidebar'>
-      <h1> I'm a side bar</h1>
       <div className='sidebar_header'>
         <div className='sidebar_headerRight'></div>
+        <IconButton>
+          <AllOutIcon />
+        </IconButton>
       </div>
     </div>
   );
