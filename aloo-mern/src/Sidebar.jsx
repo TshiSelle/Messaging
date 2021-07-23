8b552f8da82bc5163373b0c65a536cbdaba02fc1
OmarHoussami <omar.houssami20@gmail.com>
2021-07-23 19:45:46 +0300

    edited the layout of the sidebar and chat sections

--- aloo-mern/src/Sidebar.jsx
+++ aloo-mern/src/Sidebar.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import './Sidebar.css';
-import AllOutIcon from '@material-ui/icons/AllOut';
+import AllOutRoundedIcon from '@material-ui/icons/AllOutRounded';
+import ChatRoundedIcon from '@material-ui/icons/ChatRounded';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 import { Avatar, IconButton } from '@material-ui/core';
 
 function Sidebar() {
   return (
     <div className='sidebar'>
       <div className='sidebar_header'>
-        <div className='sidebar_headerRight'></div>
-        <IconButton>
-          <AllOutIcon />
-        </IconButton>
+        <Avatar src='https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png' />
+        <div className='sidebar_headerRight'>
+          <IconButton>
+            <AllOutRoundedIcon />
+          </IconButton>
+          <IconButton>
+            <ChatRoundedIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
       </div>
     </div>
   );
